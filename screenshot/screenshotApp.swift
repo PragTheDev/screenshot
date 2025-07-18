3287a9691336d39a58300c0a7507cd3edc643770
PraghilanSudhakar <73265950+PraghilanSudhakar@users.noreply.github.com>
2025-07-18 14:16:50 -0500

    init commit

---
+++ screenshot/screenshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  screenshotApp.swift
+//  screenshot
+//
+//  Created by Praghilan Sudhakar on 7/18/25.
+//
+
+import SwiftUI
+
+@main
+struct screenshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
